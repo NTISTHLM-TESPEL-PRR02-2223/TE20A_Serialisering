1bc02228eda88addb37fade7d0fea51aa2dd9ea5
Mikael Bergström <mikael.bergstrom@ga.ntig.se>
2022-10-18 13:23:15 +0200

    Initial commit

---
+++ TE20A_Serialisering/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.IO;
+using System.Text.Json;
+
+string jsonText = File.ReadAllText("talisman.json");
+
+Talisman tal = JsonSerializer.Deserialize<Talisman>(jsonText);
+
+Console.WriteLine(tal.Name);
+
+
+// Talisman tal = new Talisman()
+// {
+//   PickedUp = false,
+//   IsEquipped = false,
+//   Name = "Sprint Master",
+//   Price = 300
+// };
+
+// string talText = JsonSerializer.Serialize<Talisman>(tal);
+
+// File.WriteAllText("talisman.json", talText);
+
+// Console.WriteLine(talText);
+
+
+
+
+// string name = "Micke";
+
+// string fileName = "namn.txt";
+
+// string name = File.ReadAllText(fileName);
+
+// Console.WriteLine(name);
+
+// File.WriteAllText(fileName, name);
+
+
+
+
+Console.ReadLine();
---
+++ TE20A_Serialisering/Talisman.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class Talisman
+{
+  public bool PickedUp { get; set; }
+  public bool IsEquipped { get; set; }
+  public string Name { get; set; }
+  public int Price { get; set; }
+}
